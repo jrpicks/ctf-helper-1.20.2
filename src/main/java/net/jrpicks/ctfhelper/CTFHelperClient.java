1c63d47317e6d6ef9debd5c33c923820bac4caf1
jrpicks <ryderdaman99@gmail.com>
2023-10-16 17:07:39 -0700

    Initial commit

---
+++ src/main/java/net/jrpicks/ctfhelper/CTFHelperClient.java
@@ -0,0 +1,11 @@
+package net.jrpicks.ctfhelper;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CTFHelperClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/jrpicks/ctfhelper/CTFHelperDataGenerator.java
@@ -0,0 +1,11 @@
+package net.jrpicks.ctfhelper;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CTFHelperDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
